d4c317601be683d815d20555ef4af0ff6f0998d6
Logan McAnsh <logan@mcan.sh>
2017-10-02 16:06:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+Handlebars.registerHelper('comment_body', function () {
+  if (this.state === 'closed') {
+    return new Handlebars.SafeString(this.body);
+  }
+  return new Handlebars.SafeString(`<strong>${this.body}</strong>`);
+});
 
+function loadIssue() {
+  const issue = {
+    state: 'closed',
+    number: 5,
+    created_at: '2016-03-31 16:23:13 UTC',
+    body:
+      'Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team.',
+  };
+
+  const template = Handlebars.compile(document.querySelector('#issue-template').innerHTML);
+  const result = template(issues);
+  document.getElementsByTagName('main')[0].innerHTML += result;
+}
