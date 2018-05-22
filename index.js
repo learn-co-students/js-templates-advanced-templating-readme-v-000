cd1e07352b9f596dc7db19106c8583540a15f5b6
Sam Marshall <samuel.r.marshall@gmail.com>
2018-05-22 17:11:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 
+function loadIssue(s){
+
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+    var result = template(issues)
+    document.getElementsByTagName("main")[0].innerHTML += result;
+}
