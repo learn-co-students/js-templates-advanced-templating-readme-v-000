10ef1730e39829e33732143a9fd7f18310c2184f
github username <github email address>
2018-06-04 17:15:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+function loadIssues() {
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  var result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
+
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  var result = template(issue);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
