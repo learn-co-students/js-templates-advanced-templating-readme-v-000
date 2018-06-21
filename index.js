83883abe9a68cbed939e100e1cd324139aea4912
heislercreative <brennan@heislercreative.com>
2018-06-21 07:02:15 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+Handlebars.registerHelper('comment_body', function() {
+  if (this.state === "closed") {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+  }
+})
 
+function loadIssues() {
+    let template = Handlebars.compile(document.getElementById('issue-template').innerHTML);
+    let result = template(issues);
+
+    document.getElementsByTagName('main')[0].innerHTML += result;
+    }
+}
+
+
+// function loadIssue() {
+//   let issue = {
+//     state: "closed",
+//     number: 5,
+//     created_at: "2016-03-31 16:23:13 UTC",
+//     body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
+//   }
+//
+//   let template = Handlebars.compile(document.getElementById('issue-template').innerHTML);
+//   let result = template(issue);
+//   document.getElementsByTagName('main')[0].innerHTML += result;
+// }
