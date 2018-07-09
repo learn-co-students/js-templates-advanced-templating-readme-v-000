a09e283bb343981a714ca1d314b42d3fd50933b7
Jonathan Tiede <jon@Jonathans-MacBook-Pro.local>
2018-07-09 12:23:41 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+function loadIssues() {
 
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  for(let i = 0; i < issues.length; i++) {
+  var result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+  }
+}
