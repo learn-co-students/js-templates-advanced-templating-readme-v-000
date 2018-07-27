543ed94cddbe7d39dc0a109bb73a7a4f6fe2eae4
MiriamPeskowitz <miriam5000@gmail.com>
2018-07-26 20:49:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 
+Handlebars.registerHelper('comment_body', function() {
+  if(this.state === "closed") {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+  }
+})
+
+
+
+function loadIssues() {
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  var result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
+
+
+// function loadIssue() {
+//   // let issue = {
+//   //   state: "closed",
+//   //   number: 5,
+//   //   created_at: "2016-03-31 16:23:13 UTC",
+//   //   body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
+//   // }
+
+// //get the template
+//  let template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+
+//   for(let i=0; i<issues.length; i++) {
+//     var result = template(issues[i]);
+
+//   document.getElementsByTagName("main")[0].innerHTML += result;
+//   }
+// //pop the issue into the template, call it result 
+//  // let result = template(issue);
+
+// //put it on the page 
+//  // document.getElementsByTagName("main")[0].innerHTML += result;
+
+// } 
+
+
+// function loadIssue() {
+//   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+//   for(var i=0;i<issues.length;i++) {
+//     var result = template(issues[i]);
+//     document.getElementsByTagName("main")[0].innerHTML += result;
+//   }
+// }
