7ae01883a3c8ffd6eb72ac6c4b14c405059d00e1
Cristina Lugo <cmlugoce@syr.edu>
2018-08-21 20:02:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+function loadIssue() {
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  for(var i=0;i<issues.length;i++) {
+    var result = template(issues[i]);
+    document.getElementsByTagName("main")[0].innerHTML += result;
+  }
+}
+
+var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  var result = template(issue);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
