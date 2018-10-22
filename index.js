1372591c985a59555cfbd0b4af931196b9674484
theresamorelli <theresa.l.morelli@gmail.com>
2018-10-22 12:25:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-
-function loadIssue() {
-  var issue = {
-    state: "closed",
-    number: 5,
-    created_at: "2016-03-31 16:23:13 UTC",
-    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
+Handlebars.registerHelper('comment_body', function() {
+  if (this.state === "closed") {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
   }
-  
-  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  var result = template(issue);
+})
+
+function loadIssues() {
+  const template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+
+  const result = template(issues);
   document.getElementsByTagName("main")[0].innerHTML += result;
 }
