b77f30f47c4d38824d4566c957da1388f36e6b81
Naomi Dennis <njdennis94@gmail.com>
2018-11-05 18:03:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+let info = { aIssues : issues }
+let src = document.getElementById("issue-log").innerHTML 
+let template = Handlebars.compile(src)
+
+
+document.getElementById("main").innerHTML += template(info) 
+
