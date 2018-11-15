8d7aa6a5b766e737707cccfb689e4efcb935fe5b
ElleeB <laura.trager@gmail.com>
2018-11-15 12:21:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-function loadIssue() {
-  let issue = {
-    state: "closed",
-    number: 5,
-    created_at: "2016-03-31 16:23:13 UTC",
-    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
+Handlebars.registerHelper('comment_body', function() {
+  if(this.state === "closed") {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
   }
+})
+
+function loadIssues() {
   // compile markup and delimeters as part of a function we call next
   let template = Handlebars.compile(document.getElementById("issue-template").innerHTML)
 
-  // result = template + the above issue to render the HTML string
-  let result = template(issue)
+  // result = template + passing the entire array of issues from issues.js
+  let result = template(issues)
 
   // append it to an element within our page so we can see it
   document.getElementsByTagName("main")[0].innerHTML += result
