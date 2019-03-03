e759938616e9b33b969cbb523552a3a8d95447f6
github username <github email address>
2019-03-03 11:18:59 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+Handlebars.registerHelper('comment_body', function() {
+  if(this.state === "closed") {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+  }
+})
 
+function loadIssues() {
+
+  let template = Handlebars.compile(document.querySelector('#issue-template').innerHTML);
+
+  let result = template(issues);
+
+  document.querySelector('main').innerHTML += result;
+
+}
